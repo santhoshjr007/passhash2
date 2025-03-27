88ffbb944a2be121a99ac8c6b83535c27af37122
santhoshjr007 <santhosh.r@kalvium.community>
2025-03-27 12:51:25 +0530

    fonr

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const { resolve } = require('path');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
 const app = express();
+app.use(express.json());
 const port = 3010;
 
-app.use(express.static('static'));
+require('dotenv').config(); 
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log('Connected to MongoDB'))
+.catch((err)=>console.log(err));
+const UserSchema=new mongoose.Schema({
+  username:{type:String,required:true},
+  password:{type:String,required:true}
+})
+const User=mongoose.model('User',UserSchema);
+
+app.post('/login',async(req,res)=>{
+  const {username,password}=req.body;
+  if (!username||!password){
+    return(res.status(400).json({message:'Invalid input'}));
+  }
+  try{
+    const user=await User.findOne({username});
+    if(!user){
+      return(res.status(400).json({message:'Invalid username or password'}));
+    }
+    const ismatch=await bcrypt.compare(password,user.password)
+      if(!ismatch){
+        return(res.status(400).json({message:"Invalid credentials"}));
+
+      }
+      res.status(200).json({message:"Login Succerssful"});
+  }catch (err){
+    res.status(500).json({message:"Internal server error"});
+  }
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
